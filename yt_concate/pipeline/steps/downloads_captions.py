5ddfdc28ed0e5b250db8125b22714bb1bdd6a604
Robert Chien <robertmadhead0919@gmail.com>
2021-08-29 00:41:54 +0800

    2021/8/28

--- yt_concate/pipeline/steps/downloads_captions.py
+++ yt_concate/pipeline/steps/downloads_captions.py
@@ -8,16 +8,17 @@ from yt_concate.pipeline.steps.step import Step
 
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
+        logger=logging.getLogger('logs')
         start=time.time()
         for yt in data:
             if utils.caption_file_exist(yt):
-                print('Caption File Has already downloaded')
+                logger.warning('Caption File Has already downloaded')
                 continue
             try:
                 srt = YouTubeTranscriptApi.get_transcript(yt.id, languages=['en'])
                 #print(srt)
             except:
-                print('Error when downloading caption for', yt.url)
+                logger.error('Error when downloading caption for', yt.url)
                 continue
             # save the caption to a file named Output.txt
             with open(yt.caption_filepath, "w", encoding='utf-8') as text_file:
@@ -25,7 +26,7 @@ class DownloadCaptions(Step):
                     text_file.write(f"{caption['start']}-->{caption['start'] + caption['duration']:.2f}\n")
                     text_file.write(f"{caption['text']}\n")
         end=time.time()
-        print(end-start)
+        logger.debug(end-start)
         return data
 
 
